2196dce4c9ff0cf312e36e609714a84c3830491d
rtm0x143 <russia-artyom@mail.ru>
2023-11-08 23:40:55 +0700

    init

---
+++ Controller.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleApp1;
+
+[ApiController]
+public class MyController : ControllerBase
+{
+    private readonly IAuthorizationService _service;
+
+    public MyController(IAuthorizationService service)
+    {
+        _service = service;
+    }
+
+    [Authorize("MyPolicy", AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+    [HttpPost("/route")]
+    public string MyMethod()
+    {
+        return "Hello World";
+    }
+
+    public async Task<IActionResult> MethodWithService()
+    {
+        var result = await _service.AuthorizeAsync(User, null,
+            new AssertionRequirement(context => context.User.Identity?.Name == "rtm0x143"));
+
+        if (!result.Succeeded) return new StatusCodeResult(StatusCodes.Status403Forbidden);
+
+        return Ok("Hello World");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿var builder = WebApplication.CreateBuilder();
+
+builder.Services.AddAuthentication()
+    .AddCookie();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("MyPolicy", policyBuilder =>
+    {
+        policyBuilder.RequireRole("admin");
+        policyBuilder.RequireClaim("claimType", "claimValue1", "claimValue1");
+    });
+});
+
+builder.Services.AddControllers();
+
+var webApplication = builder.Build();
+
+webApplication.UseAuthentication()
+    .UseAuthorization();
+
+webApplication.MapControllers();
+
+webApplication.Run();
